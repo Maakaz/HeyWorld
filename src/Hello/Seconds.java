dac76af668616eb688d1960c2e346bcef4fce82c
Maakaz <rainerelp1@gmail.com>
2016-10-19 16:41:01 +0300

    uus komm

--- src/Hello/Seconds.java
+++ src/Hello/Seconds.java
@@ -7,13 +7,17 @@ import java.util.Scanner;
  */
 public class Seconds {
     public static void main() {
+        int minute = 60;
+        int second = 60;
         Scanner se = new Scanner(System.in);
         System.out.println("Enter some seconds: ");
         int sec = se.nextInt();
+        int mod_h = sec%(minute*second);
+        int hour = (sec - mod_h)/(minute*second);
 
-
-
-        System.out.println("Those Celsius in Farhenheit: " );
+        int second_2 = mod_h % second;
+        int minute_3 = (mod_h - second_2)/(second);
+        System.out.printf("%d seconds = %d hours, %d minutes and %d seconds ", sec, hour, minute_3, second_2);
 
     }
 }
